5a0fab2a6368984b7d7f8631713055badb6db029
HemantMotwani02 <87947295+HemantMotwani02@users.noreply.github.com>
2024-06-05 13:38:35 +0530

    Add files via upload

---
+++ build/precache-manifest.1eb7764ce1e594b37ba50e439433965b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2588432949aa8c10108e",
+    "url": "/LGM-VIP-Web-dev-task2/static/css/main.3a350806.chunk.css"
+  },
+  {
+    "revision": "2588432949aa8c10108e",
+    "url": "/LGM-VIP-Web-dev-task2/static/js/main.25884329.chunk.js"
+  },
+  {
+    "revision": "4f1dff4311f08c107f6e",
+    "url": "/LGM-VIP-Web-dev-task2/static/js/1.4f1dff43.chunk.js"
+  },
+  {
+    "revision": "d51485e961534c6a25e0",
+    "url": "/LGM-VIP-Web-dev-task2/static/js/runtime~main.d51485e9.js"
+  },
+  {
+    "revision": "9fe646d8f4c8c07703362d7377617564",
+    "url": "/LGM-VIP-Web-dev-task2/index.html"
+  }
+];
